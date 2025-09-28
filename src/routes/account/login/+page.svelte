a36832626dd38125a03e42329b276f474ce4dfac
ht-coding <harrie.teague@gmail.com>
2025-09-28 14:52:26 +0100

    update to use new components

--- src/routes/account/login/+page.svelte
+++ src/routes/account/login/+page.svelte
@@ -2,33 +2,17 @@
 	import { enhance } from '$app/forms';
 	import type { ActionData } from './$types';
 
+	import { Button } from '$lib/components/ui/button';
+	import { Input } from '$lib/components/ui/input';
+
 	let { form }: { form: ActionData } = $props();
 </script>
 
 <h1>Login/Register</h1>
 <form method="post" action="?/login" use:enhance>
-	<label>
-		Username
-		<input
-			name="username"
-			class="mt-1 rounded-md border border-gray-300 bg-white px-3 py-2 shadow-sm focus:border-blue-500 focus:ring-2 focus:ring-blue-500 focus:outline-none"
-		/>
-	</label>
-	<label>
-		Password
-		<input
-			type="password"
-			name="password"
-			class="mt-1 rounded-md border border-gray-300 bg-white px-3 py-2 shadow-sm focus:border-blue-500 focus:ring-2 focus:ring-blue-500 focus:outline-none"
-		/>
-	</label>
-	<button class="rounded-md bg-blue-600 px-4 py-2 text-white transition hover:bg-blue-700"
-		>Login</button
-	>
-	<button
-		formaction="?/register"
-		class="rounded-md bg-blue-600 px-4 py-2 text-white transition hover:bg-blue-700"
-		>Register</button
-	>
+	<Input name="username" placeholder="Username" class="mt-1 inline max-w-xs" />
+	<Input type="password" name="password" placeholder="password" class="mt-1  inline max-w-xs" />
+	<Button type="submit">Login</Button>
+	<Button type="submit" formaction="?/register">Register</Button>
 </form>
 <p style="color: red">{form?.message ?? ''}</p>
