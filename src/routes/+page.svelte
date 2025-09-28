c03f423ea77bac5531c6333d2289b366147912eb
ht-coding <harrie.teague@gmail.com>
2025-09-28 19:05:14 +0100

    style landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	import Input from '$lib/components/ui/input/input.svelte';
+	import IconMagnifyingGlassBold from 'phosphor-icons-svelte/IconMagnifyingGlassBold.svelte';
+	// export let data;
+</script>
+
+<h1 class="py-5 text-center text-2xl">Welcome to Launchpad Museum</h1>
+
+<form class="flex w-full items-center justify-center space-x-2">
+	<Input type="text" placeholder="Search" class=" max-w-sm" />
+	<Button type="submit">
+		<IconMagnifyingGlassBold />
+	</Button>
+</form>
