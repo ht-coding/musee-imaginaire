ec3737b355a997e8519a234ce9b9d70c6f07c5ac
ht-coding <harrie.teague@gmail.com>
2025-10-01 18:14:43 +0100

    write fetchMetData() & parseMetData() and pass all tests

--- src/lib/server/utils.ts
+++ src/lib/server/utils.ts
@@ -54,3 +54,55 @@ export async function parseHarvardData(records: HarvardRecord[]): Promise<Artwor
 		description: record.description ?? 'Visit the Harvard Art Museums website for more information'
 	}));
 }
+
+function delay(ms: number) {
+	return new Promise((resolve) => {
+		setTimeout(resolve, ms);
+	});
+}
+
+type MetRecord = {
+	primaryImage: string;
+	objectURL: string;
+	accessionYear: string;
+	creditLine: string;
+	department: string;
+	title: string;
+	medium: string;
+};
+type MetRecordWithId = MetRecord & { id: number };
+
+export async function fetchMetData(numberOfRecords: number): Promise<MetRecordWithId[]> {
+	const metApiUrl = 'https://collectionapi.metmuseum.org/public/collection/v1/objects';
+	const metResponse = await fetch(
+		`${metApiUrl}?departmentIds=11&hasImages=true&isPublicDomain=true`
+	);
+	const { objectIDs } = await metResponse.json();
+
+	const results = [];
+	for (const id of objectIDs.slice(0, numberOfRecords)) {
+		await delay(35);
+
+		const response = await fetch(`${metApiUrl}/${id}`);
+		const record: MetRecord = await response.json();
+		const recordWithId: MetRecordWithId = { id, ...record };
+		results.push(recordWithId);
+	}
+	return results;
+}
+
+export async function parseMetData(records: MetRecordWithId[]): Promise<Artwork[]> {
+	return records.map((record) => ({
+		artworkId: record.id,
+		collectionId: 'Met',
+		collection: 'The Metropolitan Museum of Art',
+		imageURL: record.primaryImage,
+		artworkURL: record.objectURL,
+		accessionYear: parseInt(record.accessionYear),
+		creditLine: record.creditLine,
+		department: record.department,
+		title: record.title,
+		medium: record.medium,
+		description: 'Visit the Met website to learn more.'
+	}));
+}
