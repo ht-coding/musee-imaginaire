ed1d779c046688ba934f34c2f6e7a4a3d09d44fa
ht-coding <harrie.teague@gmail.com>
2025-10-01 18:15:00 +0100

    remove comment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,6 @@
 	import { Button } from '$lib/components/ui/button';
 	import Input from '$lib/components/ui/input/input.svelte';
 	import IconMagnifyingGlassBold from 'phosphor-icons-svelte/IconMagnifyingGlassBold.svelte';
-	// export let data;
 </script>
 
 <h1 class="py-5 text-center text-2xl">Welcome to Launchpad Museum</h1>
