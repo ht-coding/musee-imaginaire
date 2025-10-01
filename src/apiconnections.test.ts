18769e323a41b12e730b5187b8b9355a43f58705
ht-coding <harrie.teague@gmail.com>
2025-10-01 11:35:30 +0100

    write tests for getting data from the harvard art museums api

---
+++ src/apiconnections.test.ts
@@ -0,0 +1,35 @@
+import { HARVARD_API_KEY } from '$env/static/private';
+import { fetchHarvardData } from '$lib/server/utils';
+import { describe, test, expect } from 'vitest';
+
+describe('Connect to Harvard Art Museums API and fetch data', () => {
+	test('It returns an array', async () => {
+		const response = await fetchHarvardData(HARVARD_API_KEY, 1);
+		expect(Array.isArray(response)).toBe(true);
+	});
+	test('The array should only contain objects', async () => {
+		const response = await fetchHarvardData(HARVARD_API_KEY, 5);
+		response.forEach((record) => {
+			expect(typeof record).toBe('object');
+			expect(record).not.toBe(null);
+			expect(Array.isArray(record)).toBe(false);
+		});
+	});
+	test('The objects should contain the necessary keys', async () => {
+		const response = await fetchHarvardData(HARVARD_API_KEY, 5);
+		response.forEach((record) => {
+			console.log(record);
+			expect(record).toMatchObject({
+				id: expect.any(Number),
+				primaryimageurl: expect.any(String),
+				title: expect.any(String),
+				url: expect.any(String),
+				accessionyear: expect.any(Number),
+				creditline: expect.any(String),
+				division: expect.any(String),
+				medium: expect.any(String)
+			});
+			expect(typeof record.description === 'string' || record.description === null).toBe(true);
+		});
+	});
+});
