4abaf0297e63c52472da7c2cb5415b7ca338e464
ht-coding <harrie.teague@gmail.com>
2025-10-01 12:05:37 +0100

    make function recursive to ensure total records returned is exact, rename harvardResponse -> response

--- src/lib/server/utils.ts
+++ src/lib/server/utils.ts
@@ -1,3 +1,5 @@
+import type { Artwork } from '$lib/server/db/schema';
+
 type HarvardRecord = {
 	id: number;
 	primaryimageurl: string;
@@ -13,12 +15,26 @@ type HarvardRecord = {
 
 export async function fetchHarvardData(
 	apiKey: string,
-	numberOfRecords: number
+	numberOfRecords: number,
+	page: number = 1,
+	accumulatedRecords: HarvardRecord[] = []
 ): Promise<HarvardRecord[]> {
-	const harvardApiUrl = `https://api.harvardartmuseums.org/object?apikey=${apiKey}&size=${numberOfRecords}&hasimage=1&q=*&classification=Paintings`;
-	const harvardResponse = await fetch(harvardApiUrl);
-	const { records } = await harvardResponse.json();
-	return records.filter(
+	const harvardApiUrl = `https://api.harvardartmuseums.org/object?apikey=${apiKey}&size=${numberOfRecords}&hasimage=1&q=*&classification=Paintings&page=${page}`;
+	const response = await fetch(harvardApiUrl);
+	const { records } = await response.json();
+	if (!records || records.length === 0) {
+		return accumulatedRecords;
+	}
+
+	const filteredRecords = records.filter(
 		(record: HarvardRecord) => record.imagepermissionlevel === 0 && record.primaryimageurl
 	);
+
+	const finalRecords = accumulatedRecords.concat(filteredRecords);
+
+	if (finalRecords.length >= numberOfRecords) {
+		return finalRecords.slice(0, numberOfRecords);
+	}
+
+	return fetchHarvardData(apiKey, numberOfRecords, page + 1, finalRecords);
 }
