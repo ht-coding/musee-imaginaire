257396c4f20a50295e2352842dbd87ae37320134
ht-coding <harrie.teague@gmail.com>
2025-10-10 13:28:36 +0100

    Fetch data from api and begin to display artworks

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,46 @@
 <script lang="ts">
+	import { browser } from '$app/environment';
 	import { Button } from '$lib/components/ui/button';
 	import Input from '$lib/components/ui/input/input.svelte';
 	import IconMagnifyingGlassBold from 'phosphor-icons-svelte/IconMagnifyingGlassBold.svelte';
+	import { onMount } from 'svelte';
+
+	let artworksPromise: Promise<any>;
+	onMount(() => {
+		if (browser) {
+			artworksPromise = fetch('/api/artworks').then((response) => response.json());
+		}
+	});
 </script>
 
-<h1 class="py-5 text-center text-2xl">Welcome to Launchpad Museum</h1>
+<h1 class="py-5 text-center text-2xl">Your Musée Imaginaire</h1>
 
 <form class="flex w-full items-center justify-center space-x-2">
+	<!--TODO: implement fuzzy search for artworks -->
 	<Input type="text" placeholder="Search" class=" max-w-sm" />
 	<Button type="submit">
 		<IconMagnifyingGlassBold />
 	</Button>
 </form>
+{#if artworksPromise}
+	{#await artworksPromise}
+		<p>Loading...</p>
+	{:then artworks}
+		<div class="mt-5 columns-6 gap-8 px-5">
+			{#each artworks.slice(0, 24) as art}
+				<!--TODO: Pagination, figcaption styling and content, image loading placeholders -->
+				<figure class="mb-5 break-inside-avoid">
+					<img
+						src={art.thumbnailURL}
+						alt=""
+						class="shadow-sm/50"
+						style="background:#75553e;border-color: #75553e;border-style: inset inset outset outset;border-width: 0.75em;"
+					/>
+					<figcaption class="mt-auto">{art.title}</figcaption>
+				</figure>
+			{/each}
+		</div>
+	{:catch error}
+		<p>Error loading artworks: {error.message}</p>
+	{/await}
+{/if}
