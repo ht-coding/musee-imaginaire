c85fd9a055f9ca04a30e8d98b7d9d6a3a6dfe183
ht-coding <harrie.teague@gmail.com>
2025-10-17 22:20:24 +0100

    create logic for adding artworks to exhibits

--- src/routes/artworks/[id]/+page.server.ts
+++ src/routes/artworks/[id]/+page.server.ts
@@ -1,14 +1,58 @@
-import type { PageServerLoad } from '../$types';
+import { fail, redirect } from '@sveltejs/kit';
+import type { PageServerLoad, Actions } from '../$types';
+import { db } from '$lib/server/db';
+import * as table from '$lib/server/db/schema';
+import { eq } from 'drizzle-orm';
 
-export const load: PageServerLoad = async ({ fetch, params }) => {
+export const load: PageServerLoad = async ({ fetch, params, locals }) => {
 	const response = await fetch(`/api/artworks/${params.id}`);
 
 	if (!response.ok) {
 		throw new Error(`Failed to fetch artwork: ${response.status}`);
 	}
-
 	const artwork = await response.json();
+	let exhibits = [];
+	if (locals.user) {
+		const response = await fetch(`/api/users/${locals.user.username}/exhibits`);
+		if (!response.ok) {
+			throw new Error(`Failed to fetch exhibits: ${response.status}`);
+		}
+		exhibits = await response.json();
+	}
 	return {
-		artwork
+		artwork,
+		exhibits
 	};
+};
+export const actions: Actions = {
+	addToExhibit: async (event) => {
+		const formData = await event.request.formData();
+		const exhibitId = Number(formData.get('exhibitToAddTo'));
+		const artworkId = Number(formData.get('artworkId'));
+		const artworkCollectionId = formData.get('collectionId') as string;
+		if (exhibitId === 0) {
+			return fail(400, {
+				message: 'You need to select an exhibit first.'
+			});
+		}
+		const currentArtworks = await db
+			.select()
+			.from(table.exhibitToArtworks)
+			.where(eq(table.exhibitToArtworks.exhibitId, Number(exhibitId)));
+		const alreadyExists = currentArtworks.some(
+			(artwork) =>
+				artwork.artworkCollectionId === artworkCollectionId && artwork.artworkId === artworkId
+		);
+		if (alreadyExists) {
+			return fail(400, {
+				message: 'This artwork is already in that exhibit!'
+			});
+		}
+		await db.insert(table.exhibitToArtworks).values({
+			exhibitId,
+			artworkId,
+			artworkCollectionId
+		});
+		return redirect(303, '/artworks'); // TODO: update to the exhibit when exhibit pages exist
+	}
 };
--- src/routes/artworks/[id]/+page.svelte
+++ src/routes/artworks/[id]/+page.svelte
@@ -1,6 +1,16 @@
 <script lang="ts">
+	import { enhance } from '$app/forms';
 	import Artwork from '$lib/components/Artwork.svelte';
-	const { data } = $props();
+	import Button from '$lib/components/ui/button/button.svelte';
+	import * as Select from '$lib/components/ui/select/index.js';
+	import type { Exhibit } from '$lib/server/db/schema.js';
+	const { data, form } = $props();
+	let value = $state('');
+
+	const triggerContent = $derived(
+		data.exhibits.find((exhibit: Exhibit) => exhibit.id === Number(value))?.name ??
+			'Select an exhibit'
+	);
 </script>
 
 <div class="mx-auto w-3xl">
@@ -13,6 +23,29 @@
 		{/if}{artist.name}
 	{/each}
 </h1>
+{#if data.exhibits.length > 0}
+	<form method="post" action="?/addToExhibit" use:enhance>
+		<input type="hidden" value={data.artwork.collectionId} name="collectionId" />
+		<input type="hidden" value={data.artwork.artworkId} name="artworkId" />
+		<Select.Root type="single" name="exhibitToAddTo" bind:value>
+			<Select.Trigger class="w-[180px]">
+				{triggerContent}
+			</Select.Trigger>
+			<Select.Content>
+				<Select.Group>
+					<Select.Label>Your Exhibits</Select.Label>
+					{#each data.exhibits as exhibit (exhibit.name)}
+						<Select.Item value={exhibit.id} label={exhibit.name}>
+							{exhibit.name}
+						</Select.Item>
+					{/each}
+				</Select.Group>
+			</Select.Content>
+		</Select.Root>
+		<Button type="submit">Add</Button>
+		<p style="color: red">{form?.message ?? ''}</p>
+	</form>
+{/if}
 <p>{data.artwork.description}</p>
 <p>
 	<a href={data.artwork.artworkURL} class="text-blue-600 hover:underline"
