07892b1c5ebab00db6c4767842bc0f5f1aac8ba1
ht-coding <harrie.teague@gmail.com>
2025-10-17 22:23:38 +0100

    remove unneeded api route

--- src/routes/api/exhibits/+server.ts
+++
@@ -1,8 +0,0 @@
-import { db } from '$lib/server/db';
-import * as table from '$lib/server/db/schema';
-import { json } from '@sveltejs/kit';
-
-export async function GET() {
-	const exhibits = await db.select().from(table.exhibit);
-	return json(exhibits);
-}
