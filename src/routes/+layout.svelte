82d057753d85bce76f0d7c4f47fba626f26040a2
ht-coding <harrie.teague@gmail.com>
2025-10-17 01:28:51 +0100

    add simple navbar styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import { page } from '$app/state';
+	import PaletteBold from 'phosphor-icons-svelte/IconPaletteBold.svelte';
 
 	let { children, data } = $props();
 </script>
@@ -16,13 +17,44 @@
 	</style>
 </svelte:head>
 
-{#if page.url.pathname !== '/account/login'}
-	<nav>
-		{#if data.user}
-			<p>Hi <a href="/account">{data.user.username}</a>!</p>
-		{:else}
-			<a href="/account/login">Login</a>
-		{/if}
-	</nav>
-{/if}
-{@render children?.()}
+<nav class="relative bg-slate-800" aria-label="Main navigation">
+	<div class="mx-auto flex h-16 items-center justify-between px-2 sm:px-6 lg:px-8">
+		<a href="/"><PaletteBold class="h-8 w-auto text-pink-500" /></a>
+
+		<menu class="me-auto hidden space-x-4 sm:ml-6 sm:flex">
+			<li role="none">
+				<a
+					href="/artworks"
+					role="menuitem"
+					aria-current={page.url.pathname === '/artworks' ? 'page' : undefined}
+					class={page.url.pathname === '/artworks'
+						? 'rounded-md bg-slate-900 px-3 py-2 text-sm font-medium text-white'
+						: 'rounded-md px-3 py-2 text-sm font-medium text-slate-300 hover:bg-white/5 hover:text-white'}
+					>Artworks</a
+				>
+			</li>
+			<li role="none">
+				<a
+					href="/exhibits"
+					role="menuitem"
+					class="rounded-md px-3 py-2 text-sm font-medium text-slate-300 hover:bg-white/5 hover:text-white"
+					>Exhibits</a
+				>
+			</li>
+		</menu>
+
+		<section class="text-slate-200 sm:ml-6" aria-label="User menu">
+			{#if data.user}
+				<p>
+					Hi <a href="/account" class="underline">{data.user.username}</a>!
+				</p>
+			{:else if page.url.pathname !== '/account/login'}
+				<a href="/account/login" class="underline">Login</a>
+			{/if}
+		</section>
+	</div>
+</nav>
+
+<main class="m-auto flex h-100 max-w-7xl flex-col">
+	{@render children?.()}
+</main>
