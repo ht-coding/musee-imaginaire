8816dd45a9b6ed9991fb9b108eaef529f5308983
ht-coding <harrie.teague@gmail.com>
2025-10-18 18:13:07 +0100

    filter and add thumbnails to exhibits

--- src/routes/exhibits/+page.server.ts
+++ src/routes/exhibits/+page.server.ts
@@ -1,10 +1,37 @@
 import { db } from '$lib/server/db';
 import * as table from '$lib/server/db/schema';
+import { and, eq } from 'drizzle-orm';
 
 export async function load() {
-	const exhibits = await db.select().from(table.exhibit);
+	const rows = await db
+		.select()
+		.from(table.exhibit)
+		.innerJoin(table.exhibitToArtworks, eq(table.exhibit.id, table.exhibitToArtworks.exhibitId))
+		.innerJoin(
+			table.artwork,
+			and(
+				eq(table.artwork.collectionId, table.exhibitToArtworks.artworkCollectionId),
+				eq(table.artwork.artworkId, table.exhibitToArtworks.artworkId)
+			)
+		)
+		.orderBy(table.exhibit.id, table.exhibitToArtworks.addedAt);
+
+	const seen = new Set<number>();
+	const filteredExhibits = rows
+		.filter((row) => {
+			if (seen.has(row.exhibit.id)) return false;
+			seen.add(row.exhibit.id);
+			return true;
+		})
+		.map((row) => ({
+			id: row.exhibit.id,
+			name: row.exhibit.name,
+			description: row.exhibit.description,
+			createdAt: row.exhibit.createdAt,
+			thumbnail: row.artwork.thumbnailURL
+		}));
 
 	return {
-		exhibits
+		exhibits: filteredExhibits
 	};
 }
